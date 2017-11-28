91c558baa4abfc834bbcd4846a9fb88c4d022244
duong ho <duonghose@gmail.com>
2017-11-27 21:16:58 -0500

    enable web application to deploy to tomcat server

--- src/main/java/com/ksu/grad/HumanResourceManagementApplication.java
+++ src/main/java/com/ksu/grad/HumanResourceManagementApplication.java
@@ -2,11 +2,13 @@ package com.ksu.grad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class HumanResourceManagementApplication {
+public class HumanResourceManagementApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HumanResourceManagementApplication.class, args);
@@ -16,5 +18,11 @@ public class HumanResourceManagementApplication {
 	 @Bean
 	 public BCryptPasswordEncoder bCryptPasswordEncoder() {
 	        return new BCryptPasswordEncoder();
+	 }
+	 
+	 @Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(HumanResourceManagementApplication.class);
+		 
 	 }
 }
